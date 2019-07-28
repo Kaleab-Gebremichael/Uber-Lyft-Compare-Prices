cb0ae3ec67b3fe3a4207f3b5454accec4b270ae1
Kaleab <kaleabsolomon470@gmail.com>
2019-07-28 13:59:32 -0700

    Set up project

---
+++ Uber-Lyft-Compare-Prices/Uber-Lyft-Compare-Prices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uber-Lyft-Compare-Prices
+//
+//  Created by Kaleab Gebremichael on 7/28/19.
+//  Copyright © 2019 Kaleab Gebremichael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Uber-Lyft-Compare-Prices/Uber-Lyft-Compare-PricesTests/Uber_Lyft_Compare_PricesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Uber_Lyft_Compare_PricesTests.swift
+//  Uber-Lyft-Compare-PricesTests
+//
+//  Created by Kaleab Gebremichael on 7/28/19.
+//  Copyright © 2019 Kaleab Gebremichael. All rights reserved.
+//
+
+import XCTest
+@testable import Uber_Lyft_Compare_Prices
+
+class Uber_Lyft_Compare_PricesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
